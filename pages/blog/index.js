ef2855fbb9c8d7aa6bf9f8079be3b17ec59a0812
Yousef <majhool.yf@gmail.com>
2021-09-19 14:10:28 -0700

    projects index style fix + full desc. page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -20,45 +20,47 @@ const Blog = ({ allPosts: { edges } }) => (
       <title>Blog articles page</title>
       <link rel='icon' href='/favicon.ico' />
     </Head>
-    <section className="pb-16 bg-blueGray-100 relative pt-32">
-      <div className="container mx-auto items-center flex flex-wrap">
+    <section className="pb-16 bg-blueGray-100 pt-32" dir="">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
         {edges.map(({ node }) => (
-          <div key={node.id} className="wrapper bg-gray-400 antialiased text-gray-900 inline-block">
+          <div key={node.id} className="m-4 bg-gray-400 antialiased text-gray-900">
             <img
               src={node.extraProjectsInfo.thumbnailImage ? node.extraProjectsInfo.thumbnailImage.mediaItemUrl : ''}
               alt={node.title}
-              className="thumbNailImage w-full object-cover object-center rounded-lg shadow-md" />
+              className="h-350-px w-full object-cover object-center rounded-lg shadow-md" />
 
-            <div className="relative px-4 -mt-16">
+            <div className="relative -mt-16">
               <div className="bg-white p-6 rounded-lg shadow-lg">
                 <div className="flex items-baseline">
-                  <span className={`${node.extraProjectsInfo.status ? "bg-teal-200" : "bg-orange-200"} text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide pt-2`}>
+                  <span className={`${node.extraProjectsInfo.status ? "bg-teal-200" : "bg-orange-200"} text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide pt-2 text-center`}>
                     {(node.extraProjectsInfo.status ? "done" : "under construction")}
                   </span>
                   <div className="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider">
-                    {node.extraProjectsInfo.floors && node.extraProjectsInfo.floors + " floors"}  &bull; {node.extraProjectsInfo.foundation && node.extraProjectsInfo.foundation + "m²"}
+                    {node.extraProjectsInfo.floors && node.extraProjectsInfo.floors + " floors"}  &nbsp; {node.extraProjectsInfo.foundation && node.extraProjectsInfo.foundation + "m²"}
                   </div>
                 </div>
 
                 <h4 className="mt-1 text-xl font-semibold uppercase leading-tight" >
                 <Link href={`/blog/${node.slug}`}>
-                      <a className="hover:text-blueGray-500 text-blueGray-700  flex items-center font-bold">
+                      <a className="text-teal-500 hover:text-lightBlue-600 flex items-center font-bold">
                         {node.title}
                       </a>
                     </Link>
                 </h4>
 
                 <div className="mt-1">
-                  overal cost: $1800
-                  <span className="text-gray-600 text-sm">   /wk</span>
+                  {/* overal cost: $1800
+                  <span className="text-gray-600 text-sm">   /wk</span> */}
                 </div>
                 <div className="mt-1">
                   employer:
                   <span className="text-gray-600 text-sm">{" " + node.extraProjectsInfo.employer}</span>
                 </div>
-                <div className="mt-4">
-                  <span className="text-md">
+                <div className="mt-4 grid ">
+                  <p>
                     project scale:
+                  </p>
+                  <span className="text-center">
                     <StarRatings rating={node.extraProjectsInfo.scale/20}/>
                   </span>
 
